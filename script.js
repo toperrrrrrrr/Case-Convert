7d428c3351500b6b1d43e40a9d0797f00403f213
toper L <toperrrrrrrr@gmail.com>
2023-04-14 16:06:14 +0800

    Some changes with the Javascript file

--- script.js
+++ script.js
@@ -1,28 +1,70 @@
-function convertToSentenceCase() {
-  let input = document.getElementById("input").value;
-  let output = input.toLowerCase().replace(/(^\s*\w|[\.\!\?]\s*\w)/g, c => c.toUpperCase());
-  document.getElementById("input").value = output;
+const input = document.getElementById('input');
+const sentenceBtn = document.getElementById('sentence-btn');
+const lowerBtn = document.getElementById('lower-btn');
+const upperBtn = document.getElementById('upper-btn');
+const capitalBtn = document.getElementById('capital-btn');
+
+// Convert input text to sentence case
+function toSentenceCase(text) {
+  return text.toLowerCase().replace(/(^|\.)\s*([a-z])/g, function(match, p1, p2) {
+    return p1 + p2.toUpperCase();
+  });
 }
 
-function convertToLowercase() {
-  let input = document.getElementById("input").value;
-  let output = input.toLowerCase();
-  document.getElementById("input").value = output;
+// Convert input text to lowercase
+function toLowerCase(text) {
+  return text.toLowerCase();
 }
 
-function convertToUppercase() {
-  let input = document.getElementById("input").value;
-  let output = input.toUpperCase();
-  document.getElementById("input").value = output;
+// Convert input text to uppercase
+function toUpperCase(text) {
+  return text.toUpperCase();
 }
 
-function convertToCapitalized() {
-  let input = document.getElementById("input").value;
-  let output = input.toLowerCase().replace(/(^\s*\w|[\.\!\?]\s*\w)/g, c => c.toUpperCase());
-  document.getElementById("input").value = output;
+// Convert input text to capitalized
+function toCapitalized(text) {
+  const words = text.split(' ');
+  const capitalizedWords = words.map(function(word) {
+    if (word.length > 0) {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    } else {
+      return word;
+    }
+  });
+  return capitalizedWords.join(' ');
 }
 
-document.getElementById("sentence-btn").addEventListener("click", convertToSentenceCase);
-document.getElementById("lower-btn").addEventListener("click", convertToLowercase);
-document.getElementById("upper-btn").addEventListener("click", convertToUppercase);
-document.getElementById("capital-btn").addEventListener("click", convertToCapitalized);
+// Copy text to clipboard
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
+
+// Handle button clicks
+sentenceBtn.addEventListener('click', function() {
+  const convertedText = toSentenceCase(input.value);
+  input.value = convertedText;
+  copyToClipboard(convertedText);
+});
+
+lowerBtn.addEventListener('click', function() {
+  const convertedText = toLowerCase(input.value);
+  input.value = convertedText;
+  copyToClipboard(convertedText);
+});
+
+upperBtn.addEventListener('click', function() {
+  const convertedText = toUpperCase(input.value);
+  input.value = convertedText;
+  copyToClipboard(convertedText);
+});
+
+capitalBtn.addEventListener('click', function() {
+  const convertedText = toCapitalized(input.value);
+  input.value = convertedText;
+  copyToClipboard(convertedText);
+});
